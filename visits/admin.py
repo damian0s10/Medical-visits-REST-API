ab81c90604af10ded6fbf3c0a16e1d9ed875ab02
damian0s10 <damian.wasiluk10@gmail.com>
2020-11-26 11:54:07 +0100

    added models to django admin

--- visits/admin.py
+++ visits/admin.py
@@ -5,10 +5,40 @@ from .models import *
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('user', 'pesel')
 
+
+
+@admin.register(Specjalization)
+class SpecjalizationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class SpecjalizationInline(admin.TabularInline):
+    model = Doctor.specjalizations.through
+
+
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+    list_display = ('user','about_me')
+    inlines = (SpecjalizationInline,)
+
+
+class PatientInline(admin.StackedInline):
+    model = Patient
+
+class DoctorInline(admin.StackedInline):
+    model = Doctor
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    list_display = ('id','is_patient',)
+    inlines = (PatientInline, DoctorInline)
+    list_filter = ('is_patient','is_doctor')
+
+
+@admin.register(MedicalClinic)
+class MedicalClinicAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MedicalVisit)
+class MedicalVisitAdmin(admin.ModelAdmin):
+    pass
--- visits/models.py
+++ visits/models.py
@@ -8,23 +8,47 @@ class User(AbstractUser):
     class Meta(object):
         unique_together = ('email',)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     pesel = models.CharField(max_length=11)
 
+
+
 class Specjalization(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     about_me = models.TextField()
     specjalizations = models.ManyToManyField(Specjalization)
     
+
+
+
 class MedicalClinic(models.Model):
     city = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
     local = models.CharField(max_length=10)
+    doctor = models.ForeignKey(Doctor, verbose_name='doctor_clinic', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['city']
+
+    def __str__(self):
+        return self.city +", "+ self.street + " " + self.local
+
 
 class MedicalVisit(models.Model):
     doctor = models.ForeignKey(Doctor, verbose_name='doctor_visits', on_delete=models.CASCADE)
@@ -33,3 +57,5 @@ class MedicalVisit(models.Model):
     time = models.TimeField(auto_now=False, auto_now_add=False)
     date = models.DateField(auto_now=False, auto_now_add=False)
 
+    class Meta:
+        ordering = ['date']
